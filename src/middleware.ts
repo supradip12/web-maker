2c02667c90139041c082f38451e5ae2dee0fd2d3
Supra <supradipdey1@gmail.com>
2024-02-17 23:02:19 +0530

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+// test
 export default authMiddleware({
   publicRoutes: ["/site", "/api/uploadthing"],
   async beforeAuth(auth, req) {},
